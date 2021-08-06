f11cd30eef89fcfe644be475f801dbedcddf379f
Aarón Mejía Alfaro <aaronmejiaalfaro@gmail.com>
2021-08-05 20:43:24 -0500

    Update ValuesController.cs

--- DemoNetcore/Controllers/ValuesController.cs
+++ DemoNetcore/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace DemoNetcore.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2","values3" };
         }
 
         // GET api/values/5
